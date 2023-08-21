9a4dfe84c608b262b371abe7ea8f33425a04351a
HC-Gwon <gurckddlz@naver.com>
2023-08-21 22:35:35 +0900

    leetcode 11번 문제

---
+++ leetcode/javascript/11. Container With Most Water.js
@@ -0,0 +1,17 @@
+var maxArea = function (height) {
+  let answer = 0;
+  let left = 0;
+  let right = height.length - 1;
+
+  while (left < right) {
+    answer = Math.max(
+      answer,
+      (right - left) * Math.min(height[left], height[right])
+    );
+
+    if (height[left] < height[right]) left += 1;
+    else right -= 1;
+  }
+
+  return answer;
+};
